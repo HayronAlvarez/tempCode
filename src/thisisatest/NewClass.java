00342b0d17869e77c356aed239f8d631b9c5c21f
halva023 <halva023@jccl-17.ad.cs.fiu.edu>
2019-11-13 12:09:40 -0500

    new class

---
+++ src/thisisatest/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thisisatest;
+
+/**
+ *
+ * @author halva023
+ */
+public class NewClass {
+    
+}
